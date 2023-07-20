7d4a97346ca50bd8571615003d03622730c0a970
davidovicd <davidovicd620@gmail.com>
2023-07-20 16:49:30 +0200

    Added plan for the game

---
+++ main.js
@@ -0,0 +1,8 @@
+// get computers choice
+  // do not put the computers choice or players choice to the console, just return them
+
+// get players choice
+
+// compare choices
+  // compare two choices, and print the winner on the console
+
