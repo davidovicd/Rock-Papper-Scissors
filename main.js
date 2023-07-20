0bfe7ef0a644eeb8bb084b83eba4ac10b695a27c
davidovicd <davidovicd620@gmail.com>
2023-07-20 17:29:59 +0200

    Compared two values, and called a function to declare a winner

--- main.js
+++ main.js
@@ -1,12 +1,55 @@
 // get computers choice
 function getComputersChoice(){
-  // do not put the computers choice  to the console, just return the value
   let options = ["rock", "papper", "scissors"]
   let computersChoice = options[Math.floor(Math.random() * options.length)]
+  console.log(computersChoice);
   return computersChoice
 }
 
 // compare choices
+function playRound(playersChoice, computersChoice){
+  // do not put the winer to the console  to the console, just return the value
+
+  // player choose rock
+  if (playersChoice.toLowerCase() == "rock"){
+    if (computersChoice == "rock"){
+      return `It's a draw`
+    }
+    else if (computersChoice == "papper"){
+      return `You lose papper beats rock`
+    }
+    else {
+      return `You win, rock crushes scissors`
+    }
+  } 
+  
+  // player choose papper
+  else if (playersChoice.toLowerCase() == "papper"){
+    if (computersChoice == "papper"){
+      return `It's a draw`
+    }
+    else if (computersChoice == "rock"){
+      return `You win papper beats rock`
+    }
+    else {
+      return `You lose, scissors cut papper`
+    }
+  }
+  
+  // player choose scissors
+  else {
+    if (computersChoice == "scissors"){
+      return `It's a draw`
+    }
+    else if (computersChoice == "rock") {
+      return `You lose, rock crushes scissors`
+    }
+    else {
+      return `You win, scissors cut papper`
+    }
+  }
+}
+
 
 
 // get players choice
@@ -14,4 +57,5 @@ const playersChoice = prompt("Please insert your choice, rock, papper or scissor
 const computersChoice = getComputersChoice()
 
 // print the winner on the console
+console.log(playRound(playersChoice, computersChoice));
 
